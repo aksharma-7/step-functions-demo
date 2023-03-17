f0f9dd2dfa32dcd7cb68334f61ff58dff7f035fb
aksharma-7 <atulkumar.sharma711@gmail.com>
2023-03-17 23:16:40 +0530

    Added state machine logic and state machine for error handling

---
+++ handler.js
@@ -0,0 +1,44 @@
+const AWS = require("aws-sdk");
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient({ region: "us-east-2" });
+
+const isBookAvailable = (book, quantity) => {
+  return book.quantity - quantity;
+};
+
+const checkInventory = async ({ bookId, quantity }) => {
+  try {
+    let params = {
+      TableName: "bookTable",
+      KeyConditionExpression: "bookId = :bookId",
+      ExpressionAttributeValues: {
+        ":bookId": bookId,
+      },
+    };
+
+    let result = await DocumentClient.query(params).promise();
+    const book = result.Items[0];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The book is out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+  } catch (error) {
+    if (error.name === "BookOutOfStock") {
+      throw error;
+    } else {
+      let bookNotFoundError = new Error(error);
+      bookNotFoundError.name = "BookNotFound";
+      throw bookNotFoundError;
+    }
+  }
+};
+
+const calculateTotal = async (event) => {
+  return 100;
+};
+
+module.exports = { checkInventory, calculateTotal };
